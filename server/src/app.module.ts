758ad993a8fe1dee75c9a95b846ad34108bd704e
ar <selion.forc@gmail.com>
2022-07-02 14:05:31 +0200

    full init (but without redux implementation)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
-    
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [],
+            autoLoadModels: true
+        }),
+    ]
 })
 export class AppModule {
-
+    
 }
+
--- server/src/main.ts
+++ server/src/main.ts
@@ -6,5 +6,7 @@ async function start() {
     const PORT = process.env.PORT || 5000
     const app = await NestFactory.create(AppModule)
 
-    await app.listen(PORT, () => console.log('Started'))
+    await app.listen(PORT, () => console.log(`Started server on ${PORT}`))
 }
+
+start()
